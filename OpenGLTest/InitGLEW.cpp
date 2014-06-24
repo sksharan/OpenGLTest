934a058910eee79f72e46f15b6b74117d8f14263
sksharan <shaneel.sharan@gmail.com>
2014-06-23 23:48:25 -0700

    caught a bug where the glewInit() function causes the GL_INVALID_ENUM error

--- OpenGLTest/InitGLEW.cpp
+++ OpenGLTest/InitGLEW.cpp
@@ -2,9 +2,12 @@
 #include <GL/glew.h>
 #include <iostream>
 
+#include "Util.h"
+
 bool initGLEW() {
 	glewExperimental = GL_TRUE;
 	GLenum status = glewInit();
+	checkGLError(false);  //toss the GL_INVALID_ENUM error
 	if (status != GLEW_OK) {
 		fprintf(stderr, "Error initializing GLEW: %s\n", glewGetErrorString(status));
 		return false;
