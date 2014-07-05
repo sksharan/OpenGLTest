cf5183eff1a7d5ad2495c2fa068367fc3a347e70
sksharan <shaneel.sharan@gmail.com>
2014-07-04 23:39:22 -0700

    added new functionality that allows the user to toggle between objects and modify them individually

--- OpenGLTest/KeyHandler.cpp
+++ OpenGLTest/KeyHandler.cpp
@@ -10,6 +10,18 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
 
+/* Keyboard-related controls:
+	ESC : end program
+	W, A, S, D : move the viewer
+	Q : select previous object
+	E : select next object
+	R : switching rendering mode of the currently selected object
+	T : toggle wireframe mode on/off
+	Z : change to translate mode (then manipulate object with ALL ARROW KEYS, LSHIFT, SPACEBAR)
+	X : change to scale mode (then manipulate object with LEFT AND RIGHT ARROW KEYS)
+	C : change to rotate mode (then manipulate object with ALL ARROW KEYS)
+*/
+
 #define KEYHANDLER_DEBUG 0
 
 const Uint8* keystate = SDL_GetKeyboardState(NULL);
@@ -21,6 +33,11 @@ bool wireframeEnabled = false;
 RenderableObject* currObject;
 /* 'currObject' is RenderableObject::getRenderableObjects()[objectIndex]. */
 unsigned int objectIndex = 0;
+
+/* An enum for the different transformations that a user can make to an object. */
+enum Transformation {
+	TRANSLATE, SCALE, ROTATE
+} transformationMode;
 
 /* Function protoypes. */
 void handleObjects(SDL_Event event);
@@ -106,13 +123,14 @@ void handleObjects(SDL_Event event) {
 
 	/* If we have no current object, select the first object as the current object. */
 	if (currObject == NULL) {
-		currObject = RenderableObject::getRenderableObjects()[objectIndex];
+		currObject = RenderableObject::getRenderableObjects()[0];
+		printf("%s now selected\n", currObject->getName().c_str());
 	}
 
 	switch (event.key.keysym.sym) {
 
-	/* UP ARROW: go to next object. */
-	case SDLK_UP:
+	/* E: go to next object. */
+	case SDLK_e:
 		if (objectIndex == numRenderableObjects - 1) {
 			objectIndex = 0;
 		} else {
@@ -122,8 +140,8 @@ void handleObjects(SDL_Event event) {
 		printf("%s now selected\n", currObject->getName().c_str());
 		break;
 
-	/* DOWN ARROW: go to previous object. */
-	case SDLK_DOWN:
+	/* Q: go to previous object. */
+	case SDLK_q:
 		if (objectIndex == 0) {
 			objectIndex = numRenderableObjects - 1;
 		}
@@ -138,9 +156,97 @@ void handleObjects(SDL_Event event) {
 	case SDLK_r:
 		if (currObject->getRenderMode() == RENDERMODE_NORMALS) {
 			currObject->setRenderMode(RENDERMODE_TEXTURED);
+			printf("changing render mode of %s to RENDERMODE_TEXTURED\n", currObject->getName().c_str());
 		} else {
 			currObject->setRenderMode(RENDERMODE_NORMALS);
+			printf("changing render mode of %s to RENDERMODE_NORMALS\n", currObject->getName().c_str());
 		}
+		break;
+
+	/* Z: switch to translate mode .*/
+	case SDLK_z:
+		transformationMode = TRANSLATE;
+		std::cout << "switching to translation mode" << std::endl;
+		break;
+
+	/* X: switch to scale mode .*/
+	case SDLK_x:
+		transformationMode = SCALE;
+		std::cout << "switching to scaling mode" << std::endl;
+		break;
+
+	/* C: switch to rotate mode .*/
+	case SDLK_c:
+		transformationMode = ROTATE;
+		std::cout << "switching to rotation mode" << std::endl;
+		break;
+
+	/* UP ARROW: depends on currently selected object (see Controls comment at top of file) */
+	case SDLK_UP:
+		if (transformationMode == TRANSLATE) {
+			currObject->setModelMatrix(glm::translate(currObject->getModelMatrix(), glm::vec3(0.0f, 0.0f, -translation_factor)));
+		} else if (transformationMode == SCALE) {
+			//does nothing
+		} else if (transformationMode == ROTATE) {
+			currObject->setModelMatrix(glm::rotate(currObject->getModelMatrix(), rotation_factor, glm::vec3(1.0f, 0.0f, 0.0f)));
+		}
+		break;
+
+	/* DOWN ARROW: depends on currently selected object (see Controls comment at top of file) */
+	case SDLK_DOWN:
+		if (transformationMode == TRANSLATE) {
+			currObject->setModelMatrix(glm::translate(currObject->getModelMatrix(), glm::vec3(0.0f, 0.0f, translation_factor)));
+		} else if (transformationMode == SCALE) {
+			//does nothing
+		} else if (transformationMode == ROTATE) {
+			currObject->setModelMatrix(glm::rotate(currObject->getModelMatrix(), -rotation_factor, glm::vec3(1.0f, 0.0f, 0.0f)));
+		}
+		break;
+
+	/* LEFT ARROW: depends on currently selected object (see Controls comment at top of file) */
+	case SDLK_LEFT:
+		if (transformationMode == TRANSLATE) {
+			currObject->setModelMatrix(glm::translate(currObject->getModelMatrix(), glm::vec3(-translation_factor, 0.0f, 0.0f)));
+		} else if (transformationMode == SCALE) {
+			currObject->setModelMatrix(glm::scale(currObject->getModelMatrix(), glm::vec3(1.0/scaling_factor, 1.0/scaling_factor, 1.0/scaling_factor)));
+		} else if (transformationMode == ROTATE) {
+			currObject->setModelMatrix(glm::rotate(currObject->getModelMatrix(), -rotation_factor, glm::vec3(0.0f, 1.0f, 0.0f)));
+		}
+		break;
+
+	/* RIGHT ARROW: depends on currently selected object (see Controls comment at top of file) */
+	case SDLK_RIGHT:
+		if (transformationMode == TRANSLATE) {
+			currObject->setModelMatrix(glm::translate(currObject->getModelMatrix(), glm::vec3(translation_factor, 0.0f, 0.0f)));
+		} else if (transformationMode == SCALE) {
+			currObject->setModelMatrix(glm::scale(currObject->getModelMatrix(), glm::vec3(scaling_factor, scaling_factor, scaling_factor)));
+		} else if (transformationMode == ROTATE) {
+			currObject->setModelMatrix(glm::rotate(currObject->getModelMatrix(), rotation_factor, glm::vec3(0.0f, 1.0f, 0.0f)));
+		}
+		break;
+
+    /* LSHIFT : depends on currently selected object (see Controls comment at top of file) */
+	case SDLK_LSHIFT:
+		if (transformationMode == TRANSLATE) {
+			currObject->setModelMatrix(glm::translate(currObject->getModelMatrix(), glm::vec3(0.0f, -translation_factor, 0.0f)));
+		} else if (transformationMode == SCALE) {
+			//does nothing
+		} else if (transformationMode == ROTATE) {
+			//does nothing
+		}
+		break;
+
+	/* SPACEBAR : depends on currently selected object (see Controls comment at top of file) */
+	case SDLK_SPACE:
+		if (transformationMode == TRANSLATE) {
+			currObject->setModelMatrix(glm::translate(currObject->getModelMatrix(), glm::vec3(0.0f, translation_factor, 0.0f)));
+		} else if (transformationMode == SCALE) {
+			//does nothing
+		} else if (transformationMode == ROTATE) {
+			//does nothing
+		}
+		break;
+
 	default:
 		break;
 	}
