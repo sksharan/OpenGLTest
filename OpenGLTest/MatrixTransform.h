157947283e5a9800e4d83a80c3b35e1bd15c3cee
sksharan <shaneel.sharan@gmail.com>
2014-07-04 23:36:27 -0700

    comment changes

--- OpenGLTest/MatrixTransform.h
+++ OpenGLTest/MatrixTransform.h
@@ -3,18 +3,20 @@
 #ifndef MATRIXTRANSFORM_H
 #define MATRIXTRANSFORM_H
 
-/* Sets up the perspective matrix for use in the current shaders.*/
+/* Sets up the perspective matrix for use in the current shaders, using the perspective matrix
+in 'programState' (see ProgramState.h). */
 void updateUniformPerspective();
 
-/* Sets up the model matrix for use in the current shaders. Updates the normal matrix
-in the CPU (ProgramState.h) and GPU as well. */
+/* Sets up the model matrix for use in the current shaders, using the model matrix
+in 'programState' (see ProgramState.h). Updates normal matrix as well. */
 void updateUniformModel();
 
-/* Sets up the view matrix for use in the current shaders. Updates the normal matrix
-in the CPU (ProgramState.h) and GPU as well. */
+/* Sets up the view matrix for use in the current shaders, using the view matrix
+in 'programState' (see ProgramState.h). Updates normal matrix as well. */
 void updateUniformView();
 
-/* Sets up the normal matrix for use in the current shaders. */
+/* Sets up the normal matrix for use in the current shaders, using the normal matrix
+in 'programState' (see ProgramState.h). */
 void updateUniformNormal();
 
 /* Updates the normal matrix in ProgramState based on the current model and
